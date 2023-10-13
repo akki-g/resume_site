e45fcf9cd1f629023af1bce15b3f0cbf029c97aa
akki-g <146907426+akki-g@users.noreply.github.com>
2023-10-13 10:55:07 -0400

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-n^+km#2=d0j_hfhh)r5s41_f&vw(+n6t*1qt+8ax1t=$e+&2n@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '18.117.110.167.'
+]
+
 
 
 # Application definition
